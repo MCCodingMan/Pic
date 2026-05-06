d0ad0aafdaeeb5438446419235ba9fa6ab322bd6
Wan_CodingMan <wmc912@163.com>
2026-05-06 11:28:56 +0800

    delete files

--- StreamingDepthDataFromTheTrueDepthCamera/TrueDepthStreamer/TrueDepthStreamer-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-/*
-See the LICENSE.txt file for this sample’s licensing information.
-
-Abstract:
-Bridging header for TrueDepthStreamer
-*/
-
-#import "HistogramCalculator.h"
-#import "PointCloudMetalView.h"
