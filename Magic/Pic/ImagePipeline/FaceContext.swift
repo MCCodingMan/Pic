87de1f3a041a890cd205f4f97a74459ce8eb5f5c
Wan_CodingMan <wmc912@163.com>
2026-05-30 21:44:04 +0800

    merge
    commit e09f69156bdf89f76f169b75f433673a7d6c295a
    Author: Wan_CodingMan <‘wmc912@163.com’>
    Date:   Sat May 30 21:42:59 2026 +0800
    
        main to test
    
        commit 1ac061e0d744c398bdeca9187d45c3f300c6b524
        Author: Wan_CodingMan <‘wmc912@163.com’>
        Date:   Sat May 30 21:42:20 2026 +0800
    
            B
    
        commit 5aaacc4219ac16a71f4fec413b5d26c8e2daa1c0
        Author: Wan_CodingMan <‘wmc912@163.com’>
        Date:   Sat May 30 21:40:47 2026 +0800
    
            a
    
        commit a2414d3126502d88da14bb11e645514878622dcb
        Author: Wan_CodingMan <‘wmc912@163.com’>
        Date:   Sat May 30 21:39:28 2026 +0800
    
            Squashed commit of the following:
    
            commit bfdea205b8d422b25e59d7dd16ec91e54863b114
            Author: Wan_CodingMan <‘wmc912@163.com’>
            Date:   Sat May 30 21:38:48 2026 +0800
    
                8
    
            commit 3f8ca97deb0c43cafcbe697245a15d0ccb11f6a7
            Author: Wan_CodingMan <‘wmc912@163.com’>
            Date:   Sat May 30 21:38:42 2026 +0800
    
                7
    
            commit 093ea5b21a5ac7720da6be2d02cc654d8d83e35e
            Author: Wan_CodingMan <‘wmc912@163.com’>
            Date:   Sat May 30 21:38:34 2026 +0800
    
                6
    
            commit 43edb36e2415c0f443f82e2933e09abcb60ef88e
            Author: Wan_CodingMan <‘wmc912@163.com’>
            Date:   Sat May 30 21:38:26 2026 +0800
    
                5
    
            commit 343a595541e6d1416f68e007684b67f3c2aab082
            Author: Wan_CodingMan <‘wmc912@163.com’>
            Date:   Sat May 30 21:38:19 2026 +0800
    
                4
    
            commit 1a10e5bbef6c2d12902de19f5517fc2e7382e987
            Author: Wan_CodingMan <‘wmc912@163.com’>
            Date:   Sat May 30 21:38:13 2026 +0800
    
                3
    
            commit bbc08b6bf478fdf9402ca909265a929c84db48c5
            Author: Wan_CodingMan <‘wmc912@163.com’>
            Date:   Sat May 30 21:38:02 2026 +0800
    
                2
    
            commit ddb1dfa732acfcdb7a71aa4e7380d920147f994c
            Author: Wan_CodingMan <‘wmc912@163.com’>
            Date:   Sat May 30 21:37:44 2026 +0800
    
                1
    
    commit eeb5c127ff3edda7d8edc83487b01d9715ac2bd4
    Author: Wan_CodingMan <‘wmc912@163.com’>
    Date:   Sat May 30 21:41:26 2026 +0800
    
        b
    
    commit bfdea205b8d422b25e59d7dd16ec91e54863b114
    Author: Wan_CodingMan <‘wmc912@163.com’>
    Date:   Sat May 30 21:38:48 2026 +0800
    
        8
    
    commit 3f8ca97deb0c43cafcbe697245a15d0ccb11f6a7
    Author: Wan_CodingMan <‘wmc912@163.com’>
    Date:   Sat May 30 21:38:42 2026 +0800
    
        7
    
    commit 093ea5b21a5ac7720da6be2d02cc654d8d83e35e
    Author: Wan_CodingMan <‘wmc912@163.com’>
    Date:   Sat May 30 21:38:34 2026 +0800
    
        6
    
    commit 43edb36e2415c0f443f82e2933e09abcb60ef88e
    Author: Wan_CodingMan <‘wmc912@163.com’>
    Date:   Sat May 30 21:38:26 2026 +0800
    
        5
    
    commit 343a595541e6d1416f68e007684b67f3c2aab082
    Author: Wan_CodingMan <‘wmc912@163.com’>
    Date:   Sat May 30 21:38:19 2026 +0800
    
        4
    
    commit 1a10e5bbef6c2d12902de19f5517fc2e7382e987
    Author: Wan_CodingMan <‘wmc912@163.com’>
    Date:   Sat May 30 21:38:13 2026 +0800
    
        3
    
    commit bbc08b6bf478fdf9402ca909265a929c84db48c5
    Author: Wan_CodingMan <‘wmc912@163.com’>
    Date:   Sat May 30 21:38:02 2026 +0800
    
        2
    
    commit ddb1dfa732acfcdb7a71aa4e7380d920147f994c
    Author: Wan_CodingMan <‘wmc912@163.com’>
    Date:   Sat May 30 21:37:44 2026 +0800
    
        1

--- Magic/Pic/ImagePipeline/FaceContext.swift
+++ Magic/Pic/ImagePipeline/FaceContext.swift
@@ -9,7 +9,9 @@ nonisolated struct FaceContext: Sendable {
         case vision        // 高精度 Vision (含 landmarks)
         case avMetadata    // 实时 AVCaptureMetadataOutput (仅 box)
         case none
-    }    
+    }
+    
+    
 
     struct Face: @unchecked Sendable {
         /// 归一化坐标 (Vision 坐标系：原点左下，y 向上)
